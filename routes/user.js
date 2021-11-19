10396dbdfbd355ee305302834dcfd360e328a5bb
KARIMANSHAWKI <kariman0800@gmail.com>
2021-11-20 00:11:55 +0200

    user crud

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,8 @@
 const router = require("express").Router();
+const { updateUser, deleteUser, getUser } = require("../controllers/users");
 
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
+router.get("/:id", getUser);
 
-
-module.exports = router
+module.exports = router;
