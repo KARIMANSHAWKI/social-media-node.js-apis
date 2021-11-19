b062e92d1a58bd103fb542c0dbbc37bdc54b42bf
KARIMANSHAWKI <kariman0800@gmail.com>
2021-11-20 01:39:30 +0200

    seperate mongo config

--- index.js
+++ index.js
@@ -13,15 +13,6 @@ const postRoute = require("./routes/post");
 //load config
 dotenv.config({ path: "./config/config.env" });
 
-// connect MongoDB
-mongoose.connect(
-  process.env.MONGO_URI,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  () => {
-    console.log("db connected");
-  }
-);
-
 // init server
 const app = express();
 
