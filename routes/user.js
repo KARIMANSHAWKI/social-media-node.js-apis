38fb3790e10ce1b7574b23873d041d066d4af8ae
KARIMANSHAWKI <kariman0800@gmail.com>
2021-11-20 00:31:15 +0200

    follow un follow user

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
-const { updateUser, deleteUser, getUser } = require("../controllers/users");
+const { updateUser, deleteUser, getUser , unFollowUser, followUser} = require("../controllers/users");
 
 router.put("/:id", updateUser);
 router.delete("/:id", deleteUser);
 router.get("/:id", getUser);
+router.put("/:id/follow", followUser);
+router.put("/:id/unfollow", unFollowUser);
 
 module.exports = router;
